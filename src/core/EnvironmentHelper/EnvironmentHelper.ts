349ae3a1d22b604ab81b735fc23902db807c32a8
Kamil <kamil@arch.localdomain>
2019-08-12 22:13:06 +0200

    Remove unused classes

--- src/core/EnvironmentHelper/EnvironmentHelper.ts
+++
@@ -1,6 +0,0 @@
-export class EnvironmentHelper {
-   public isNode() {
-      const isNode = new Function("try {return this===global;}catch(e){return false;}")
-      return isNode()
-   }
-}
--- src/core/StringHelper/StringHelper.ts
+++
@@ -1,5 +0,0 @@
-export class StringHelper {
-   public readStringUntil(text: string, untilChar: string) {
-      return text.substring(0, text.indexOf(untilChar))
-   }
-}
